ec13f097fed5f55b15b32a3935c40bc6cfcd7a04
AntonZenin <zenin.anton.2005@gmail.com>
2026-04-18 02:10:31 +0300

    added tests and changed a little bit

--- llvm/compiler-course/backend/NullPass/NullPass.cpp
+++ llvm/compiler-course/backend/NullPass/NullPass.cpp
@@ -1,10 +1,9 @@
 #include "X86.h"
-#include "X86InstrBuilder.h" 
 #include "X86InstrInfo.h"
 #include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/IR/DebugInfoMetadata.h" 
+#include "llvm/IR/DebugInfoMetadata.h"
 
 using namespace llvm;
 
@@ -15,34 +14,38 @@ public:
   NullCheckPass() : MachineFunctionPass(ID) {}
   bool runOnMachineFunction(MachineFunction &MF) override;
 
-private: 
-  bool readsFromMemory(const MachineInstr &MI) {
-    return MI.mayLoad() && !MI.isCall();
+private:
+  bool isInterestingLoad(const MachineInstr &MI) {
+    return MI.mayLoad() && !MI.isCall() && !MI.isBranch();
   }
 
   Register getBaseReg(const MachineInstr &MI) {
     const MCInstrDesc &desc = MI.getDesc();
     int memOpStart = X86II::getMemoryOperandNo(desc.TSFlags);
-    if (memOpStart < 0) {
-      return 0;
-    }
-    memOpStart += desc.getNumDefs();
+    if (memOpStart < 0)
+      return Register();
 
     const MachineOperand &baseOp = MI.getOperand(memOpStart);
-    if (!baseOp.isReg() || baseOp.getReg() == 0) {
-      return 0;
-    }
+    if (!baseOp.isReg())
+      return Register();
+
     return baseOp.getReg();
-
   }
 
-  MachineBasicBlock *createNullHandlerBlock(MachineFunction &MF, const X86InstrInfo *TII) {
-    MachineBasicBlock *nullBlock = MF.CreateMachineBasicBlock();
-    MF.push_back(nullBlock);
-    BuildMI(nullBlock, llvm::DebugLoc(), TII->get(X86::TRAP));
+  MachineBasicBlock *getOrCreateTrapBlock(MachineFunction &MF,
+                                          const X86InstrInfo *TII) {
+    if (TrapBlock)
+      return TrapBlock;
 
-    return nullBlock;
+    TrapBlock = MF.CreateMachineBasicBlock();
+    MF.push_back(TrapBlock);
+    BuildMI(TrapBlock, DebugLoc(), TII->get(X86::TRAP));
+
+    return TrapBlock;
   }
+
+private:
+  MachineBasicBlock *TrapBlock = nullptr;
 };
 
 char NullCheckPass::ID = 0;
@@ -52,55 +55,48 @@ bool NullCheckPass::runOnMachineFunction(MachineFunction &MF) {
   const X86InstrInfo *TII = subtarget.getInstrInfo();
   bool changed = false;
 
-  std::vector<MachineInstr*> instructionsToCheck;
+  SmallVector<MachineInstr *, 16> workList;
   for (auto &MBB : MF) {
     for (auto &MI : MBB) {
-      if (readsFromMemory(MI) && getBaseReg(MI) != 0) {
-        instructionsToCheck.push_back(&MI);
-      }
+      if (isInterestingLoad(MI) && getBaseReg(MI))
+        workList.push_back(&MI);
     }
   }
 
-  for (auto *MI : instructionsToCheck) {
+  for (MachineInstr *MI : workList) {
     MachineBasicBlock *currMBB = MI->getParent();
+    DebugLoc DL = MI->getDebugLoc();
+
     Register baseReg = getBaseReg(*MI);
-    
-    if (baseReg == 0)
+    if (!baseReg)
       continue;
-    
-    
-    MachineBasicBlock *nullBlock = createNullHandlerBlock(MF, TII);
+    MachineBasicBlock *trapBlock = getOrCreateTrapBlock(MF, TII);
     MachineBasicBlock *continueBlock = MF.CreateMachineBasicBlock();
-    
-    
     MF.insert(std::next(MachineFunction::iterator(currMBB)), continueBlock);
 
-    continueBlock->splice(continueBlock->begin(), currMBB,
-                          MI->getIterator(), currMBB->end());    
-    
+    continueBlock->splice(continueBlock->begin(), currMBB, MI->getIterator(),
+                          currMBB->end());
+
     continueBlock->transferSuccessors(currMBB);
-  
+    while (!currMBB->succ_empty())
+      currMBB->removeSuccessor(currMBB->succ_begin());
     currMBB->addSuccessor(continueBlock);
-    currMBB->addSuccessor(nullBlock);
-    
-    
-    BuildMI(currMBB, DebugLoc(), TII->get(X86::TEST64rr))
+    currMBB->addSuccessor(trapBlock);
+
+    BuildMI(*currMBB, currMBB->end(), DL, TII->get(X86::TEST64rr))
         .addReg(baseReg)
         .addReg(baseReg);
-    BuildMI(currMBB, DebugLoc(), TII->get(X86::JCC_1))
-        .addMBB(nullBlock)
+    BuildMI(*currMBB, currMBB->end(), DL, TII->get(X86::JCC_1))
+        .addMBB(trapBlock)
         .addImm(X86::COND_E);
-    
-    BuildMI(currMBB, DebugLoc(), TII->get(X86::JMP_1))
-    .addMBB(continueBlock);
-    
+    BuildMI(*currMBB, currMBB->end(), DL, TII->get(X86::JMP_1))
+        .addMBB(continueBlock);
     changed = true;
   }
-  
+
   return changed;
 }
 } // namespace
 
-static RegisterPass<NullCheckPass> X("null-check", "Null pointer check pass", false,
-                                   false);
-
+static RegisterPass<NullCheckPass> X("null-check", "Null pointer check pass",
+                                     false, false);
